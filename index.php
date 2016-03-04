fc0a236c852ed07ba7f03739f7676301991b2c75
larrygeams.nst <larrygeamsparangan.nst@gmail.com>
2016-03-04 15:28:51 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * The main template file
+ * 
+ *
+ * @package Wired
+ * @version 1.1.0
+ */
+
+include "header.php";
+include "nav.php"; ?>
+
+
+<div id="primary" class="content-area">
+    <main class="site-main" class="site-main" role="main">
+
+      <div class="container"> 
+         <h1 class="bold mg-b-30">Wired template</h1>
+         <p class="fs-18 semi-bold">Basic template we use as starting point while coding a resource project</p>
+      </div>
+      
+    </main>
+</div>
+
+<?php include "footer.php" ?>
