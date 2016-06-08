a8b1da3b9d3798340bd5738fefbee5690276c96e
larrygeams.nst <larrygeamsparangan.nst@gmail.com>
2016-06-08 13:59:42 -0800

    Updated package

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# bootstrap-gulp
-Bootstrap 4 Sass with Gulp
+# Bootstrap Gulp
+Shim repository for Twitter Bootstrap 4 using Gulp
+
+This package just provides the basic Bootstrap 4 in the package. This means that although the stylesheets are there, you will be required to provide your own means of including the styles. 
+
+The only thing about this package is all Scss files are compiled using Gulp instead of Grunt.
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,15 @@
 var gulp 		     = require('gulp'),
 	 sass 		     = require('gulp-sass'),
-	 watch 		     = require('gulp-watch'),
-	 browserSync   = require('browser-sync').create();
+	 watch 		     = require('gulp-watch');
 
 gulp.task('scss', function () {
  return gulp.src('scss/*.scss')
           .pipe(sass().on('error', sass.logError))
-          //.pipe(sass({outputStyle: 'compressed'})) 
-          .pipe(gulp.dest('css'))
-          .pipe(browserSync.stream());
+          .pipe(sass({outputStyle: 'compressed'})) 
+          .pipe(gulp.dest('css'));
 });
  
 gulp.task('watch', function () { 
-   browserSync.init({ 
-        proxy: "http://larry/bootstrap-gulp" 
-    });	 
-
   gulp.watch('**/*.scss', ['scss']); 
   gulp.watch("**/*.*").on('change', browserSync.reload); 
 });
